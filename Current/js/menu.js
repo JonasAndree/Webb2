1285026a8e33021e07eba557f3ea25e136d3f06c
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-09-20 13:45:52 +0200

    2017-09-20 intro javascript

---
+++ Current/js/menu.js
@@ -0,0 +1,25 @@
+console.log("ROBIN NEJ FÖR I HELL........!!!!!!!!!!!");
+console.log("Robin skriv det här!");
+
+
+var text1 = "BLUB";
+var text2 = 'BLUB';
+var nummer1 = 23; 
+var decNumber = 32.34; 
+var boolean1 = false;
+
+console.log(boolean1 + " " + text1 + decNumber);
+console.log(nummer1 + decNumber);
+
+
+function blub() {
+	console.log("The blub function!");	
+}
+
+
+function blub2(parm) {
+	console.log(parm + text1);
+}
+
+blub();
+blub2(text2);
